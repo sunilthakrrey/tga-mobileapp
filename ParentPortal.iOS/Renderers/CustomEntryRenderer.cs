7a5886fefad11585a4575b59a1a0d6ab72d8398c
priyarana <priyarana@confianzaitsolutions.com>
2021-06-02 10:51:30 +0530

    Some changes in Custom Control

---
+++ ParentPortal.iOS/Renderers/CustomEntryRenderer.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using CoreGraphics;
+using ParentPortal.Custom.Controls;
+using ParentPortal.iOS.Renderers;
+using UIKit;
+using Xamarin.Forms;
+using Xamarin.Forms.Platform.iOS;
+
+[assembly: ExportRenderer(typeof(CustomEntry), typeof(CustomEntryRenderer))]
+namespace ParentPortal.iOS.Renderers
+{
+  public  class CustomEntryRenderer:EntryRenderer
+    {
+        protected override void OnElementChanged(ElementChangedEventArgs<Entry> e)
+        {
+            base.OnElementChanged(e);
+
+            if (e.NewElement != null)
+            {
+                var view = (CustomEntry)Element;
+                
+                Control.LeftView = new UIView(new CGRect(0f, 0f, 9f, 20f));
+                Control.LeftViewMode = UITextFieldViewMode.Always;
+                Control.KeyboardAppearance = UIKeyboardAppearance.Dark;
+                Control.ReturnKeyType = UIReturnKeyType.Done;
+                Control.Layer.BackgroundColor = view.BackgroundColor.ToCGColor();
+                Control.Layer.CornerRadius = Convert.ToSingle(view.CornerRadius);
+                Control.Layer.BorderColor = Color.Transparent.ToCGColor();
+                Control.Layer.BorderWidth = view.BorderWidth;
+                Control.ClipsToBounds = true;
+            }
+        }
+
+    }
+}
