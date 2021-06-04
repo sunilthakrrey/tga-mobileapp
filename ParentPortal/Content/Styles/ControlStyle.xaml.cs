dfaf4a62483d8d3dce1803151ca1d41d35350103
Shaylendra Patel <shaylendra@confianzaitsolutions.com>
2021-06-04 10:47:59 +0530

    changes in "DashboardView", small,medium,large and common style, created control style

---
+++ ParentPortal/Content/Styles/ControlStyle.xaml.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+
+namespace ParentPortal.Content.Styles
+{
+    [XamlCompilation(XamlCompilationOptions.Compile)]
+    public partial class ControlStyle : ContentPage
+    {
+        public static ControlStyle SharedInstance { get; } = new ControlStyle();
+        public ControlStyle()
+        {
+            InitializeComponent();
+        }
+    }
+}
