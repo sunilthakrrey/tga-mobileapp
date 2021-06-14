6174a87171fc4c44210de5fcbe97a61a5c0d61ed
Shaylendra Patel <shaylendra@confianzaitsolutions.com>
2021-06-14 10:40:15 +0530

    create splash screen

--- ParentPortal.Android/MainActivity.cs
+++ ParentPortal.Android/MainActivity.cs
@@ -8,7 +8,7 @@ using FFImageLoading.Forms.Platform;
 
 namespace ParentPortal.Droid
 {
-    [Activity(Label = "ParentPortal", Icon = "@mipmap/icon", Theme = "@style/MainTheme", MainLauncher = true, ConfigurationChanges = ConfigChanges.ScreenSize | ConfigChanges.Orientation | ConfigChanges.UiMode | ConfigChanges.ScreenLayout | ConfigChanges.SmallestScreenSize )]
+    [Activity(Label = "ParentPortal", Icon = "@mipmap/icon", Theme = "@style/MainTheme", MainLauncher = false, ConfigurationChanges = ConfigChanges.ScreenSize | ConfigChanges.Orientation | ConfigChanges.UiMode | ConfigChanges.ScreenLayout | ConfigChanges.SmallestScreenSize )]
     public class MainActivity : global::Xamarin.Forms.Platform.Android.FormsAppCompatActivity
     {
         protected override void OnCreate(Bundle savedInstanceState)
---
+++ ParentPortal.Android/SplashActivity.cs
@@ -0,0 +1,37 @@
+﻿using Android.App;
+using Android.Content;
+using Android.OS;
+using System.Threading.Tasks;
+using Xamarin.Forms.Platform.Android;
+
+namespace ParentPortal.Droid
+{
+    [Activity(Theme = "@style/MyTheme.Splash", MainLauncher = true, NoHistory = true)]
+    public class SplashActivity:FormsAppCompatActivity
+    {
+
+        public override void OnCreate(Bundle savedInstanceState, PersistableBundle persistentState)
+        {
+            base.OnCreate(savedInstanceState, persistentState);
+        }
+
+        // Launches the startup task
+        protected override void OnResume()
+        {
+            base.OnResume();
+            Task startupWork = new Task(() => { SimulateStartup(); });
+            startupWork.Start();
+        }
+        // Prevent the back button from canceling the startup process
+        public override void OnBackPressed() { }
+
+        // Simulates background work that happens behind the splash screen
+        async void SimulateStartup()
+        {
+            // Performe some startup work that takes a bit of time.
+            await Task.Delay(4000); // Simulate a bit of startup work.
+            // Startup work is finished - starting MainActivity
+            StartActivity(new Intent(Application.ApplicationContext, typeof(MainActivity)));
+        }
+    }
+}
